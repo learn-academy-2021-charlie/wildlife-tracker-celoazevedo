14d9cffa1776ad496ebf28e0146b13e04f8d54ee
celoazevedo <meazevedo86@gmail.com>
2021-08-12 10:56:26 -0700

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :animals
+  # resources :animals   <--> this will replace all of the routes that we initially had to write out. Thanks to the rails generate resourses command.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
